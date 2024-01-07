fdbd6b8fa8aedfd2f50fa8bc6963d9d9cb6eacb5
aryan <aryanchidumalla@gmail.com>
2024-01-07 15:34:28 +0530

    404 page

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import SignIn from './SignIn';
 import Dashboard from './Dashboard';
 
 import './App.css';
+import ErrorPage from './ErrorPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='*' element={<ErrorPage/>} />
         <Route path='/' element={<Navigate to="/dashboard"/>}/>
         <Route path='/dashboard' element={<Dashboard/>}/>
         <Route path='/signin' element={<SignIn/>}/>
